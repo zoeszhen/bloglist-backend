5f0ae621d2566ff6d59cf3e326875aedfcd0b3ca
SHI ZHEN <shi.zhen@vincit.fi>
2020-10-11 12:20:19 +0300

    feat(4.13-4.14): delete and update blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,32 +1,96 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 // GET http://localhost:3003/api/blogs
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
+blogsRouter.get('/', async (request, response) => {
+  try {
+    const blogs = await Blog.find({});
+    response.status(200).json(blogs);
+  } catch (error) {
+    response.status(400).json(error).end();
+  }
 });
 
 /** For rest client
-  POST http://localhost:3003/api/blogs HTTP/1.1
+  POST http://localhost:3003/api/blogs
   content-type: application/json
+
   {
-      "author": "test author1",
-      "title": "test title1",
-      "url": "https://testtitle1.com",
-      "likes": 2
+    "author": "test author3",
+    "title": "test title3",
+    "url": "https://testtitle.com1",
+    "likes": 3
   }
    */
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post('/', async (request, response) => {
+  console.log('post');
+  const { body } = request;
+  const blog = new Blog({
+    ...body,
+    likes: body.likes || 0,
+  });
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
+  if (!body.title && !body.url) {
+    response.status(400).send('Bad Request').end();
+  } else {
+    try {
+      const results = await blog.save();
+      response.status(201).json(results);
+    } catch (error) {
+      response.status(400).json(error).end();
+    }
+  }
 });
+
+/** For rest client
+  Delete http://localhost:3003/api/blogs
+  content-type: application/json
+
+  {
+    "id": "5f74c0aa7f1523ea3643b26a"
+  }
+   */
+
+blogsRouter.delete('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  const index = blogs.findIndex((item) => item.id === request.body.id);
+  try {
+    const res = await blogs[index].remove();
+    response.status(200).json(res);
+  } catch (error) {
+    response.status(400).json(error).end();
+  }
+});
+
+/** For rest client
+  PUT http://localhost:3003/api/blogs/5f7dead026a4eda64b561e1b
+  content-type: application/json
+
+  {
+    "id": "5f7dead026a4eda64b561e1b",
+    "author": "test author3",
+    "title": "test title3",
+    "url": "https://testtitle.com1",
+    "likes": 3
+  }
+   */
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const { body } = request;
+  try {
+    const res = Blog.findOneAndUpdate(
+      { _id: body.id },
+      {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+      },
+    );
+    response.status(200).json(res);
+  } catch (error) {
+    response.status(400).json(error).end();
+  }
+});
+
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,12 +4,59 @@ const app = require('../app');
 
 const api = supertest(app);
 
-test.only('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
 });
+
+test('create blogs successful', async () => {
+  const beforeCreated = await api.get('/api/blogs');
+  await api
+    .post('/api/blogs')
+    .send({
+      author: 'test author3',
+      title: 'test title3',
+      url: 'https://testtitle.com1',
+      likes: 3,
+    })
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const afterCreated = await api.get('/api/blogs');
+  expect(afterCreated.body.length - beforeCreated.body.length).toEqual(1);
+});
+
+test('If like is missing from request, should set to 0', async () => {
+  const target = await api
+    .post('/api/blogs')
+    .send({
+      author: 'test author3',
+      title: 'test title3',
+      url: 'https://testtitle.com1',
+    })
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const afterCreated = await api.get('/api/blogs');
+  const targetBlog = afterCreated.body.find(({ id }) => id === target.body.id);
+  expect(targetBlog.likes).toEqual(0);
+});
+
+test('If title or url is missing from request, should recive 400', async () => {
+  await api
+    .post('/api/blogs')
+    .send({
+      author: 'test author3',
+      likes: 1,
+    })
+    .expect(400)
+    .expect('Bad Request');
+});
+// test('Unique identifier property of the blog posts is named id ', async () => {
+//   const res = await api.get('/api/blogs');
+//   const ids = res.body.map(({ id }) => id);
+//   expect(res.body[0]["_id"]).toBeDefined();
+// });
 
 afterAll(() => {
   mongoose.connection.close();
